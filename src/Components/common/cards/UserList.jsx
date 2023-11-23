27fb669382f0d82afa123dccec6c34a92b51b5c4
GonzaloDevelop <gvarela@uade.edu.ar>
2023-11-23 20:12:33 -0300

    El Skeleton se muestra por 1 seg

--- src/Components/common/cards/UserList.jsx
+++ src/Components/common/cards/UserList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { APIContext } from '../../context/APIContextProvider'
 import Card from '../../Card'
@@ -8,15 +8,26 @@ import { Grid } from '@mui/material'
 const UserList = () => {
   const { users, isLoading } = useContext(APIContext)
   const navigate = useNavigate()
+  const [showSkeleton, setShowSkeleton] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSkeleton(false)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   const handleCardClick = (userId) => {
     navigate(`/details/${userId}`)
   }
 
+  const shouldShowSkeleton = isLoading || showSkeleton
+
   return (
     <article className='flex items-center justify-center m-auto pt-20 pb-20'>
       <Grid container alignItems='center' justifyContent='space-evenly' spacing={8} sx={{ width: '70%' }}>
-        {isLoading ? (
+        {shouldShowSkeleton ? (
           [...Array(10)].map((_, index) => (
             <Grid item xs={12} md={6} lg={4} key={index}>
               <CardSkeleton />

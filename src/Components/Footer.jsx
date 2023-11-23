e00c7bfd20a36ef404cd9fdfd4f81092b32f6220
GonzaloDevelop <gvarela@uade.edu.ar>
2023-11-23 20:41:22 -0300

    Texto Footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='flex items-center justify-center dark:bg-purple-900 text-xl dark:text-white h-10vh w-full py-6 bg-oyster-pink-300'>
       <a href='https://digitalhouse.com/ar' target='_blank' rel='noreferrer' className='flex items-center justify-center w-full h-full'>
-        <p className='pr-4 font-bold'>Powered by ➡️ Gonzalo Varela & Matias Montañez</p>
+        <p className='pr-4 font-bold dark:text-white text-lg max-w-[80vw] text-center mx-auto'>Powered by ➡️ Gonzalo Varela & Matias Montañez</p>
       </a>
     </footer>
   )

af4239b0d01aa26b34a260e0e303351500048c79
GonzaloDevelop <gvarela@uade.edu.ar>
2023-11-30 18:27:03 -0300

    Botón Go Back agregado

--- src/Routes/NotFoundPage.jsx
+++ src/Routes/NotFoundPage.jsx
@@ -1,4 +1,5 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
 
@@ -6,9 +7,13 @@ const NotFoundPage = () => {
         document.title = '404 - Página no encontrada'
     }, [])
 
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
-      <div className="text-center">
+    const handleSupportClick = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+            <div className="text-center">
         <div className="mb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 text-yellow-400 mx-auto" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM11 7h2v6h-2V7zm0 8h2v2h-2v-2z"/>
@@ -17,9 +22,17 @@ const NotFoundPage = () => {
         <h1 className="text-6xl font-bold text-red-600 mb-4">404</h1>
         <p className="text-lg font-medium text-gray-300 mb-8">Página no encontrada</p>
         <p className="text-gray-400">Lo sentimos, la página que buscas no existe o fue movida.</p>
-      </div>
-    </div>
-  )
+                <div className="flex flex-wrap items-center justify-center gap-3 mt-6">
+                    <Link to="/" className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg">
+                        Go back
+                    </Link>
+                    <a href="/" onClick={handleSupportClick} className="block py-2 px-4 text-white hover:bg-green-600 font-medium duration-150 active:bg-gray-100 border rounded-lg">
+                        Contact support
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NotFoundPage
